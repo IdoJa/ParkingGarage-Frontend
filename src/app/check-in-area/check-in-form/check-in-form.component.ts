017335697b74208cc92c5dbe4dd2913e8e7d9f0a
Ido <idoja@gmail.com>
2021-11-28 05:16:29 +0200

    after changing grid height for check-in form

--- src/app/check-in-area/check-in-form/check-in-form.component.ts
+++ src/app/check-in-area/check-in-form/check-in-form.component.ts
@@ -33,7 +33,7 @@ export class CheckInFormComponent implements OnInit {
 
 
 
-  public async test(vehicleFormInfo: NgForm) {
+  public async getAllTickets(vehicleFormInfo: NgForm) {
     let selectedVehicleName = vehicleFormInfo.controls['class'].value;
     try {
 
@@ -43,79 +43,5 @@ export class CheckInFormComponent implements OnInit {
     catch (err) {
       console.log(err);
     }
-
-
-
-
-
-
-
-    // this.reset();
-    
-    // let selectedVehicleClassValue = vehicleFormInfo.controls['class'].value;
-    // console.log(selectedVehicleClassValue);
-    // let vehicleListCopy = { ...this.vehiclesList };
-
-    // console.log(this.vehiclesList);
-    // console.log(vehicleListCopy);
-    // console.log(this.vehiclesList);
-    // console.log(this.vehiclesList.indexOf(selectedVehicleClassValue));
-    // console.log("Length ", this.vehiclesList.length);
-    // console.log(vehicleListCopy[this.vehiclesList.indexOf(selectedVehicleClassValue)]);
-
-    // let selectionIndex = this.vehiclesList.indexOf(selectedVehicleClassValue);
-    
-    // // from 'Motorcycle' to 'Van' show 'Vip', 'Regular'
-    // if (selectionIndex >= 0 && selectionIndex < this.vehiclesList.length - 1) {
-    //   this.ticketList.pop();
-      
-    // }
-
-    // console.log(this.ticketList);
-
-
-    
-  }
-
-
-  public checkSelectedVehicle(vehicleFormInfo: NgForm) {
-    let selectedVehicleClassValue = vehicleFormInfo.controls['class'].value;
-    let vehicleListCopy = this.vehiclesList;
-    let ticketsToShow = this.ticketsList;
-    let counter = 0;
-
-    vehicleListCopy.pop();
-
-    console.log(this.ticketsList);
-    // remove 'Regular' from ticketList if selectedVehicle is from 'Motorcycle' to 'Van'
-    // vehicleListCopy.forEach((e) => selectedVehicleClassValue === e ? ticketsToShow.pop() : 'b');
-    //vehicleListCopy.forEach((e) => this.test(selectedVehicleClassValue, e));
-
-    // vehicleListCopy.forEach((e) => {
-    //   if (selectedVehicleClassValue === e) {
-    //     ticketsToShow.pop();
-    //   } else {
-    //     selectedVehicleClassValue === 'B';
-    //     ticketsToShow.pop();
-    //   }
-    // });
-
-    vehicleListCopy.includes('Truck');
-
-    console.log(vehicleListCopy);
-    console.log(ticketsToShow);
-
-
-
-    //console.log(this.ticketList);
-   // console.group(ticketsToShow);
-
-    // if (selectedVehicleClassValue === 'Motorcycle' ) {
-
-    // }
-      
-    console.log(counter);
-      
-      // else if value != null then VIP
   }
 }
