92303a70e118a15ce5ad50c34bd5294a50b52b47
Ido <idoja@gmail.com>
2021-11-28 05:02:56 +0200

    after renaming to parking garage

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Northwind
+# Parkinggarage
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.4.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('Northwind app is running!');
+    expect(await page.getTitleText()).toEqual('Parkinggarage app is running!');
   });
 
   afterEach(async () => {
