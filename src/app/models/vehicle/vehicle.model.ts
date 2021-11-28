eb3e4c27d0bc714b70934612954e7847794b642c
Ido <idoja@gmail.com>
2021-11-28 06:07:40 +0200

    Before changing vehicle model

--- src/app/models/vehicle/vehicle.model.ts
+++ src/app/models/vehicle/vehicle.model.ts
@@ -1,6 +1,8 @@
 export class Vehicle {
     public licensePlateId: string;
-    public class: string;
+    public name: string;
+    public phone: string;
+    public class: string; // vehicleName
     public ticket: string;
     public height: number;
     public width: number;
